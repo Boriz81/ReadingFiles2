bd8178263197750b9274ac574037fdce0fa266c6
Boriz81 <blitzboriz@gmail.com>
2023-01-18 21:50:14 +0900

    working

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -13,41 +14,23 @@ public class Main {
     }
     public static void main(String[] args) throws IOException {
         // Reading Files
-        File file = new File("C:\\Users\\user\\IdeaProjects\\Reading Files>\\text.txt");
-        try (Scanner scanner2 = new Scanner(file)) {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNext()) {
-                System.out.println(scanner.nextLine() + " ");
+        String string = new String(Files.readAllBytes(Paths.get("C:\\Users\\user\\IdeaProjects\\Reading Files\\code.txt")));
+
+        String[] myArray = string.split("\\s\\W");
+        //System.out.println(myArray.length);
+        int count = 0;
+
+
+        for (int a = 0; a < myArray.length; a++) {
+            if (Integer.parseInt(myArray[a]) == 0) {
+                System.out.println(count);
+                break;
+            } else if (Integer.parseInt(myArray[a]) % 2 == 0) {
+                count++;
+
             }
-            scanner = new Scanner(file);
-        } catch (Exception e) {
-            System.out.println(e.getCause());
-        }
-        File file2 = new File("C:\\system.tmp");
-        // каноничный пример
-        try (Scanner scanner2 = new Scanner(file)) {
-            while (scanner2.hasNext()) {
-                System.out.println(scanner2.nextLine() + " ");
-            }
-        } catch (FileNotFoundException e) {
-
-            System.out.println("No file found: " + file2);
         }
 
-        // работаем с многостроковым текстом
-        String pathToFileInUsers = "C:\\Users\\user\\folderForFiles\\text.txt";
-        // вызываю из метода
-        System.out.println(readFileAsString(pathToFileInUsers));
-        // вызываю непосредственно
-        try {
-            System.out.println(new String(Files.readAllBytes(Paths.get(pathToFileInUsers))));
-        } catch (Exception e) {
-            System.out.println(e.getCause());
         }
 
-        // запись в файл
-        String message = "Added text";
-        Path path = Path.of("C:\\Users\\user\\folderForFiles\\text.txt");
-        Files.writeString(path, message);
-    }
 }
